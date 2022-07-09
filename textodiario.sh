675b2b550611715d3c4eb3f044e44b9b3d826211
Oscar <oscar.paternina@gmail.com>
2022-07-08 19:53:07 -0500

    Primer commit

---
+++ textodiario.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Fuente: Cursos de Youtube
+# Elaborado por: Oscar Paternina
+# Julio 8 de 2022 - Barranquilla
+# Titulo: Diario de textos en bash V.01
+
+
+# Carpetas asignadas manualmene
+Carpeta_Actual=DATA		
+Archivo_Actual=Julio2022
+Configuracion=config
+
+# definicion del menu inicial
+Menu_Inicial () {
+	cd $Carpeta_Actual
+	echo "============================"
+	echo "TEXTO DIARIO EN BASH"
+	echo "----------------------------"
+	echo "1 - Crear Carpeta"
+	echo "2 - Crear Archivo"
+	echo "3 - Editar archivo"
+	echo "4 - Configuracion"
+	echo "5 - Salir"
+	echo "============================"
+	echo "Carpeta nueva: "$carpeta
+	echo "Archivo nuevo: "$archivo
+	echo "----------------------------"
+	echo "Carpeta actual: " $Carpeta_Actual
+	echo "Archivo actual: " $Archivo_Actual
+	echo "============================"
+}
+
+# Esta funcion crea una carpeta en el directorio y cambia a ella
+Menu_Crear_Carpeta () {
+	read -p "Ingrese el nombre para la carpeta: " carpeta
+	mkdir -m 777 $carpeta
+	cd $carpeta
+	echo "Carpeta creada."
+	sleep 2
+	clear
+	Menu_Opcion
+}
+
+# Esta funcion crea un archivo en el directorio donde este la nueva carpeta
+Menu_Crear_Archivo () {
+	read -p "Ingrese el nombre del arhivo: " archivo
+	touch $archivo
+	echo "Archivo $archivo creado"
+	sleep 2
+	clear
+	Menu_Opcion
+}
+
+# Esta funcion abre la carpeta creada y edita el archivo con nano
+Menu_Editar_Archivo () {
+	gedit $Archivo_Actual
+	echo "Archivo $Archivo_Actual actualizado."
+	sleep 3
+	clear
+	Menu_Opcion
+}
+
+Menu_Conf () {
+	#echo $Carpeta_Actual
+	echo ""
+	echo "------------------------------------------------"
+	echo "Carpeta y archivos en directorio $Configuracion"
+	cat $Configuracion
+	echo "------------------------------------------------"
+	echo ""
+	echo "------------------------------------------------"
+	echo "Directorio actual: "
+	pwd
+	echo "------------------------------------------------"
+	read -p "Pulse a para Menu - b para Salir: " volver
+	case $volver in
+		"a") Menu_Opcion;;
+		"b") Menu_Salir;;
+		[a-b]) echo "Ha ingresado un valor fuera del rango";;
+		*) echo "Por favor solo caracteres dentro del rango [a-b]"
+	esac
+}
+
+# Esta funcion temina la ejecucion del programa
+Menu_Salir () {
+	echo "Saliendo del Script..."
+	sleep 2
+	clear
+}
+
+# Esta funcion ejecuta las opciones del menu inicial
+Menu_Opcion () {
+clear
+Menu_Inicial
+read -p "Ingrese la opción deseada: " opcion
+case $opcion in
+	"1") Menu_Crear_Carpeta;;
+	"2") Menu_Crear_Archivo;;
+	"3") Menu_Editar_Archivo;;
+	"4") Menu_Conf;;
+	"5") Menu_Salir;;
+	[1-5]) echo "Ha ingresado un valor fuera del rango";;
+	*) echo "Por favor solo caracteres dentro del rango [1-5]"
+esac
+}
+Menu_Opcion
